358d2cba0a86f8d38795cbd84649d316ebb19226
rammne <rametiongson@gmail.com>
2025-12-22 20:46:00 +0800

    added dummy data for turbo to work

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,17 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
+  integrations: [react(), sitemap()],
+  site: 'https://www.olopsc.edu.ph',
+
   vite: {
     plugins: [tailwindcss()]
-  },
-
-  integrations: [react(), sitemap()],
-  site: 'https://www.olopsc.edu.ph'
+  }
 });
