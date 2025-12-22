dddf7c33839da6380d152c1198cf9aff0857817a
rammne <rametiongson@gmail.com>
2025-12-22 14:10:04 +0800

    added configs for web and sitemaps

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [react()]
+  integrations: [react(), sitemap()],
+  site: 'https://www.olopsc.edu.ph'
 });
